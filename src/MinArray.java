28fb6558a66d0b0c9af676c41fdeded2ce918f09
Maslik-dnepr <maslik2002@gmail.com>
2021-12-05 20:14:30 +0200

    initial

---
+++ src/MinArray.java
@@ -0,0 +1,20 @@
+public class MinArray {
+
+	public static void main(String[] args)
+	{
+		int[] at={22,55,33,22,8,1,45,7};
+		System.out.println(funcMin(at));
+		       
+	}
+	
+	static int funcMin(int[] ar)
+	{
+		int result=ar[0];
+		for (int i=1;i<ar.length;i++)
+		{
+			result=(ar[i]<result)?ar[i]:result;
+		}
+		return result;
+	}
+
+}
